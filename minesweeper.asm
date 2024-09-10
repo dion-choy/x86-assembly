835a0f5583c499530f33bfd7c3cd6b3832361e6b
dion-choy <dionchoynj@gmail.com>
2024-09-10 16:31:28 +0800

    added win lose screen

--- minesweeper.asm
+++ minesweeper.asm
@@ -3,12 +3,17 @@ org 100h
 
 jmp drawField
 
-bomb equ 162
-flag equ 16
-screen1 equ 0
-screen2 equ 1
+mineChar equ 162
+flagChar equ 16
+display equ 0
+minefield equ 1
 numOfBombs equ 99
 numOfRevealed dw 0
+pRNG db 0
+
+winMessage db "You WIN!!", 0Dh, 0Ah, "$"
+loseMessage db "You Lose", 0Dh, 0Ah, "$"             
+lastMessage db "Press any key to close...", 0Dh, 0Ah, "$"
 
 drawField:      ; draw minefield
 mov ax, 3
@@ -21,27 +26,19 @@ int 10h
 
 mov dh, 3
 mov dl, 25     
-mov bh, screen2     ; <=======change back to page 1 to hide
+mov bh, minefield     ; <=======change back to page 1 to hide
 mov cx, 16
-drawZeros:      ; draw vertical sides
-push cx
-
-inc dh
-mov ah, 2
-int 10h
-
-mov ah, 0Ah
 mov al, '0'
-mov cx, 30
-int 10h
-
-pop cx
-loop drawZeros
+call fillWithChar
 
 ; load number of mines
 mov cx, numOfBombs
 populateMines:
 push cx
+
+mov ah, 2Ch
+int 21h             ; interrupt to get sys time
+mov pRNG, dl
 
 rerollCoords:
 
@@ -51,11 +48,10 @@ mov ax, 0
 
 mov bh, 16
 mov al, dl
+add al, pRNG
 div bh
 mov bh, ah          ; modulus so row is 0-16
 add bh, 4
-
-call delay
 
 mov ah, 2Ch
 int 21h             ; interrupt to get sys time
@@ -63,22 +59,23 @@ mov ax, 0
 
 mov bl, 30
 mov al, dl
+add al, pRNG
 div bl
 mov bl, ah          ; modulus so col is 0-30
 add bl, 25 
 
 mov dx, bx  
-mov bh, screen2     ; <=======change back to page 1 to hide
+mov bh, minefield     ; <=======change back to page 1 to hide
 mov ah, 2
 int 10h             ; interrupt to move cursor
 
 mov ah, 8
 int 10h 
-cmp al, bomb         ; check if occupied
+cmp al, mineChar         ; check if occupied
 je rerollCoords
 
 
-mov al, bomb         ; if no mine, draw
+mov al, mineChar         ; if no mineChar, draw
 mov ah, 09h  
 mov bl, 0F0h       ; attribute black colour
 mov cx, 1
@@ -88,12 +85,37 @@ call incSides
 pop cx
 loop populateMines
 
-mov bh, screen1
+mov bh, display
 call drawBorder
 
-mov bh, screen2
+mov bh, minefield
 call drawBorder
+
+mov dh, 3
+mov dl, 25     
+mov bh, display     ; <=======change back to page 1 to hide
+mov cx, 16
+mov al, ' '
+call fillWithChar
+
 jmp allowInput
+
+; SETUP PROCEDURES
+fillWithChar proc
+    fillWithCharLoop:      ; draw vertical sides
+    push cx
+    
+    inc dh
+    mov ah, 2
+    int 10h
+    
+    mov ah, 0Ah
+    mov cx, 30
+    int 10h
+    pop cx
+    loop fillWithCharLoop
+    ret
+fillWithChar endp
 
 
 drawBorder proc     ; draw minefield
@@ -145,18 +167,8 @@ drawBorder proc     ; draw minefield
     ret
 drawBorder endp
 
-delay proc
-    push dx
-    mov ah, 86h
-    mov cx, 0h
-    mov dx, 053h
-    int 15h
-    pop dx
-    ret
-delay endp
-
 incSides proc
-    mov cl, bomb
+    mov cl, mineChar
     
     dec dh
     dec dl
@@ -220,12 +232,13 @@ incTiles proc
     pop cx
     ret
 incTiles endp
+; END SETUP PROCEDURES
 
 allowInput:     ; start user input
 mov ax, 0
 int 33h
 
-again:
+again:          ; GAMEPLAY LOOP
 mov ax, 3
 int 33h
 
@@ -238,7 +251,7 @@ xchg ax, dx
 div cl
 
 mov dh, al
-mov bh, screen1
+mov bh, display
 mov ah, 2
 int 10h
 
@@ -247,7 +260,7 @@ cmp bl, 1
 je revealTile
 
 cmp bl, 2
-je placeFlag
+je placeFlagChar
 
 ; Check if WIN condition
 ; i.e. numOfTiles - numOfRevealed = numOfBombs
@@ -256,93 +269,29 @@ sub ax, numOfRevealed
 cmp ax, numOfBombs
 je winExit
 
-;call debugger
-
 jmp again
-
-
-;debugger proc
-;    push ax
-;    push bx
-;    push cx
-;    push dx
-;    
-;    mov dx, 0
-;    mov bh, screen1
-;    mov ah, 2
-;    int 10h
-;    
-;    mov al, numOfRevealed     ; empty box
-;    mov ah, 09h  
-;    mov bl, 0Fh 
-;    mov cx, 1
-;    int 10h
-;    
-;    pop dx
-;    pop cx
-;    pop bx
-;    pop ax
-;    ret
-;debugger endp
-
-check proc
-    mov ah, 2
-    int 10h
-    
-    mov ah, 8
-    int 10h
-    
-    cmp al, cl
-    ret
-check endp
-
-copyTile proc
-    push cx
-    mov bh, screen2
-    mov ah, 2
-    int 10h
-    
-    mov ah, 8
-    int 10h
-    mov bl, ah
-    
-    mov bh, screen1
-    mov ah, 2
-    int 10h
-    
-    mov ah, 09h
-    mov bh, screen1
-    mov cx, 1
-    int 10h
-    
-    inc numOfRevealed
-    pop cx      
-    ret
-copyTile endp
 
 ; LEFT CLICK FUNCTION
 revealTile:
 
-mov cl, flag
-mov bl, screen1
+mov cl, flagChar
+mov bl, display
 call check
 je leftMouseDown
 
 mov cl, ' '
-mov bl, screen1
+mov bl, display
 call check
 jne leftMouseDown
 
 call copyTile
 
-cmp al, bomb
+cmp al, mineChar
 je loseExit
 
 cmp al, '0'
 jne leftMouseDown
-call delay
 call revealZeros
-call delay
 
 leftMouseDown:
 mov ax, 3
@@ -352,12 +301,12 @@ je leftMouseDown
 jmp again
 
 ; RIGHT CLICK FUNCTION
-placeFlag:
+placeFlagChar:
 
-mov cl, flag
-mov bl, screen1
+mov cl, flagChar
+mov bl, display
 call check
-jne flagTile
+jne flagCharTile
 
 mov al, ' '     ; empty box
 mov ah, 09h  
@@ -367,14 +316,14 @@ int 10h
 
 jmp rightMouseDown
 
-flagTile:
+flagCharTile:
 
 mov cl, ' '
-mov bl, screen1
+mov bl, display
 call check
 jne rightMouseDown
 
-mov al, flag    ; flag
+mov al, flagChar    ; flagChar
 mov ah, 09h  
 mov bl, 1100b 
 mov cx, 1
@@ -388,18 +337,54 @@ cmp bl, 2
 je rightMouseDown
 jmp again
 
+; GAMELOOP PROCEDURES
+check proc
+    mov ah, 2
+    int 10h
+    
+    mov ah, 8
+    int 10h
+    
+    cmp al, cl
+    ret
+check endp
+
+copyTile proc
+    push cx
+    mov bh, minefield
+    mov ah, 2
+    int 10h
+    
+    mov ah, 8
+    int 10h
+    mov bl, ah
+    
+    mov bh, display
+    mov ah, 2
+    int 10h
+    
+    mov ah, 09h
+    mov bh, display
+    mov cx, 1
+    int 10h
+    
+    inc numOfRevealed
+    pop cx      
+    ret
+copyTile endp
+
 revealZeros proc
     dec dh
     
     mov cl, ' '
-    mov bh, screen1
+    mov bh, display
     call check
     jne noNZero
     
     call copyTile
     
     mov cl, '0'
-    mov bh, screen2
+    mov bh, minefield
     call check
     jne noNZero
     
@@ -410,14 +395,14 @@ revealZeros proc
     inc dh
     
     mov cl, ' '
-    mov bh, screen1
+    mov bh, display
     call check
     jne noEZero
     
     call copyTile
     
     mov cl, '0'     
-    mov bh, screen2
+    mov bh, minefield
     call check
     jne noEZero
     
@@ -428,14 +413,14 @@ revealZeros proc
     dec dl
     
     mov cl, ' '
-    mov bh, screen1
+    mov bh, display
     call check
     jne noWZero
     
     call copyTile
     
     mov cl, '0'
-    mov bh, screen2
+    mov bh, minefield
     call check
     jne noWZero
     
@@ -446,14 +431,14 @@ revealZeros proc
     inc dh
     
     mov cl, ' '
-    mov bh, screen1
+    mov bh, display
     call check
     jne noSZero
     
     call copyTile
     
     mov cl, '0'
-    mov bh, screen2
+    mov bh, minefield
     call check
     jne noSZero
     
@@ -466,10 +451,42 @@ revealZeros proc
     
     ret
 revealZeros endp
+; END GAMELOOP PROCEDURES
 
-winExit:
+showField proc
+    mov ax, 0501h   ; <=========to check if written
+    int 10h
+    
+    mov dx, 0
+    mov bh, minefield
+    mov ah, 2
+    int 10h
+    ret
+showField endp
+
+winExit: 
+call showField
+
+lea dx, winMessage
+jmp finalExit 
+
 loseExit:
-mov ax, 0501h   ; <=========to check if written
+call showField
+
+lea dx, loseMessage 
+
+finalExit:
+mov ah, 9
+int 21h
+
+lea dx, lastMessage
+mov ah, 9
+int 21h
+
+mov ah, 0
+int 16h
+
+mov ax, 3
 int 10h
 
 ret
